e322637cd73af6aa28453044fe26c6b9d17848aa
Praneeth Shetty <praneethshetty10@gmail.com>
2025-07-13 14:41:56 +0530

    fix: adding signal interupt handling

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ganimtron-10/TriFS/internal/master"
 	"github.com/ganimtron-10/TriFS/internal/transport"
@@ -12,7 +16,18 @@ func StartMaster() {
 
 	masterService := master.CreateMasterService(coreMaster)
 
-	transport.StartRpcServer(fmt.Sprintf(":%d", coreMaster.Port), masterService)
+	go func() {
+		if err := transport.StartRpcServer(fmt.Sprintf(":%d", coreMaster.Port), masterService); err != nil {
+			log.Fatalf("RPC server failed: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	log.Println("Master shutting down...")
 }
 
 func main() {
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/ganimtron-10/TriFS/internal/transport"
 	"github.com/ganimtron-10/TriFS/internal/worker"
 )
@@ -13,7 +18,18 @@ func StartWorker() {
 
 	workerService := worker.CreateWorkerService(coreWorker)
 
-	transport.StartRpcServer(coreWorker.Address, workerService)
+	go func() {
+		if err := transport.StartRpcServer(coreWorker.Address, workerService); err != nil {
+			log.Fatalf("RPC server failed: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	log.Println("Worker shutting down...")
 }
 
 func main() {
