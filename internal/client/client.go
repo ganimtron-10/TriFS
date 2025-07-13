e4e8f88a75389e0ee998c9610dec0a4121db0ad2
Praneeth Shetty <praneethshetty10@gmail.com>
2025-07-13 14:26:17 +0530

    fix: validating master resp and updating write to return err

--- internal/client/client.go
+++ internal/client/client.go
@@ -30,16 +30,16 @@ func CreateClient() *Client {
 	}
 }
 
-func (client *Client) AddConfig(config *ClientConfig) *Client {
-	client.ClientConfig = config
-	return client
+func (c *Client) AddConfig(config *ClientConfig) *Client {
+	c.ClientConfig = config
+	return c
 }
 
-func (client *Client) Read(filename string) error {
+func (c *Client) Read(filename string) error {
 	requestArgs := &protocol.ReadFileRequestArgs{Filename: filename}
 	requestReply := &protocol.ReadFileRequestReply{}
 
-	err := transport.DialRpcCall(client.MasterAddress, "MasterService.ReadFile", requestArgs, requestReply)
+	err := transport.DialRpcCall(c.MasterAddress, "MasterService.ReadFile", requestArgs, requestReply)
 	if err != nil {
 		logger.Error(common.COMPONENT_CLIENT, fmt.Sprintf("Master ReadFile Error: %s", err))
 		return err
@@ -50,6 +50,9 @@ func (client *Client) Read(filename string) error {
 	args := &protocol.ReadFileArgs{Filename: filename}
 	reply := &protocol.ReadFileReply{}
 
+	if len(requestReply.WorkerUrls) == 0 {
+		return fmt.Errorf("no worker urls returned from master")
+	}
 	err = transport.DialRpcCall(requestReply.WorkerUrls[0], "WorkerService.ReadFile", args, reply)
 	if err != nil {
 		logger.Error(common.COMPONENT_CLIENT, fmt.Sprintf("Worker ReadFile Error: %s", err))
@@ -60,13 +63,14 @@ func (client *Client) Read(filename string) error {
 	return nil
 }
 
-func (client *Client) Write(filename, data string) {
+func (c *Client) Write(filename, data string) error {
 	requestArgs := &protocol.WriteFileRequestArgs{Filename: filename}
 	requestReply := &protocol.WriteFileRequestReply{}
 
-	err := transport.DialRpcCall(client.MasterAddress, "MasterService.WriteFile", requestArgs, requestReply)
+	err := transport.DialRpcCall(c.MasterAddress, "MasterService.WriteFile", requestArgs, requestReply)
 	if err != nil {
 		logger.Error(common.COMPONENT_CLIENT, fmt.Sprintf("Master WriteFile Error: %s", err))
+		return err
 	}
 
 	logger.Info(common.COMPONENT_CLIENT, "Master WriteFile Response", "WorkerUrl", requestReply.WorkerUrls)
@@ -74,10 +78,15 @@ func (client *Client) Write(filename, data string) {
 	args := &protocol.WriteFileArgs{Filename: filename, Data: []byte(data)}
 	reply := &protocol.WriteFileReply{}
 
+	if len(requestReply.WorkerUrls) == 0 {
+		return fmt.Errorf("no worker urls returned from master")
+	}
 	err = transport.DialRpcCall(requestReply.WorkerUrls[0], "WorkerService.WriteFile", args, reply)
 	if err != nil {
 		logger.Error(common.COMPONENT_CLIENT, fmt.Sprintf("Worker WriteFile Error: %s", err))
+		return err
 	}
 
 	logger.Info(common.COMPONENT_CLIENT, "Worker Successfully Wrote File")
+	return nil
 }
