b5c78fd334be3830db1f494cb466264251b46489
Praneeth Shetty <praneethshetty10@gmail.com>
2025-07-23 19:37:39 +0530

    fix: adding .simulate dir for easier management

--- cmd/simulate.go
+++ cmd/simulate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -13,12 +14,14 @@ import (
 )
 
 var commandList []*exec.Cmd
+var cwd string
 
 func spawn(spawnPath string) {
 	command := exec.Command("go", "run", spawnPath)
 	command.Stdout = os.Stdout
 	command.Stdin = os.Stdin
 	command.Stderr = os.Stderr
+	command.Dir = cwd
 
 	err := command.Start()
 	if err != nil {
@@ -30,9 +33,24 @@ func spawn(spawnPath string) {
 
 func main() {
 
-	masterPath := "./cmd/master/main.go"
-	workerPath := "./cmd/worker/main.go"
-	clientPath := "./cmd/client/main.go"
+	cwd = ".simulate"
+	err := os.MkdirAll(cwd, 0644)
+	if err != nil {
+		logger.Error(common.COMPONENT_COMMON, fmt.Sprintf("Unable to create directory named %s", cwd))
+		log.Fatalf("Unable to create directory named %s", cwd)
+	}
+
+	defer func() {
+		err := os.RemoveAll("./.simulate")
+		if err != nil {
+			logger.Error(common.COMPONENT_COMMON, fmt.Sprintf("Unable to delete directory named %s", cwd))
+			log.Fatalf("Unable to delete directory named %s", cwd)
+		}
+	}()
+
+	masterPath := "../cmd/master/main.go"
+	workerPath := "../cmd/worker/main.go"
+	clientPath := "../cmd/client/main.go"
 
 	spawn(masterPath)
 	time.Sleep(time.Second * 5)
