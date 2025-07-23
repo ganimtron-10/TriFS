4259ae77de31ef648a5b258cc772195a1086aab6
Praneeth Shetty <praneethshetty10@gmail.com>
2025-07-23 19:39:12 +0530

    fix: creating a seperate getFullFilePath func

--- internal/worker/handler.go
+++ internal/worker/handler.go
@@ -10,9 +10,13 @@ import (
 	"github.com/ganimtron-10/TriFS/internal/logger"
 )
 
+func (w *Worker) getFullFilePath(fileHash string) string {
+	return path.Join(w.Id, "data", fileHash)
+}
+
 func (w *Worker) handleReadFile(filename string) ([]byte, error) {
 
-	filenameHash := common.Hash(filename)
+	filenameHash := common.GetFileHash(filename)
 
 	w.fileStoreLock.RLock()
 	fileInfo := w.fileStore[filenameHash]
@@ -25,7 +29,7 @@ func (w *Worker) handleReadFile(filename string) ([]byte, error) {
 	}
 
 	// TODO: Use hashing or id gen instead of using Address
-	fullFilePath := path.Join(w.Address, filename)
+	fullFilePath := w.getFullFilePath(filenameHash)
 	file, err := os.Open(fullFilePath)
 	if err != nil {
 		logger.Error(common.COMPONENT_WORKER, fmt.Sprintf("Error while opening file named %s. Error: %s", fullFilePath, err))
@@ -52,7 +56,7 @@ func (w *Worker) handleReadFile(filename string) ([]byte, error) {
 
 func (w *Worker) handleWriteFile(filename string, data []byte) error {
 
-	filenameHash := common.Hash(filename)
+	filenameHash := common.GetFileHash(filename)
 	// TODO: Add Pack Creation and Handling Logic
 	w.fileStoreLock.Lock()
 	w.fileStore[filenameHash] = &FileInfo{
@@ -62,7 +66,7 @@ func (w *Worker) handleWriteFile(filename string, data []byte) error {
 	}
 	w.fileStoreLock.Unlock()
 
-	fullFilePath := path.Join(w.Address, filename)
+	fullFilePath := w.getFullFilePath(filenameHash)
 	if err := os.WriteFile(fullFilePath, data, 0644); err != nil {
 		logger.Error(common.COMPONENT_WORKER, fmt.Sprintf("Error while writing to file named %s. Error: %s", fullFilePath, err))
 		return err
